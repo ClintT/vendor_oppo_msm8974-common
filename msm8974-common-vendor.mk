6f448756c94fcebb7787e58f003d48ad7269e9b7
YumeMichi <do4suki@gmail.com>
2017-09-25 09:40:16 +0800

    msm8974-common: Add support for aptX & aptX HD
    Change-Id: I03d4dbf158b4657e3a2fb6fdac1460104781b78f
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- msm8974-common-vendor.mk
+++ msm8974-common-vendor.mk
@@ -74,6 +74,8 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libadiertac.so:system/vendor/lib/libadiertac.so \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libadsprpc.so:system/vendor/lib/libadsprpc.so \
+    vendor/oppo/msm8974-common/proprietary/vendor/lib/libaptXHD_encoder.so:system/vendor/lib/libaptXHD_encoder.so \
+    vendor/oppo/msm8974-common/proprietary/vendor/lib/libaptX_encoder.so:system/vendor/lib/libaptX_encoder.so \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libaudcal.so:system/vendor/lib/libaudcal.so \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libbtnv.so:system/vendor/lib/libbtnv.so \
